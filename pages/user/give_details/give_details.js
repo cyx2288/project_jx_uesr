f119e77124d10f764d00ed753dc03e1bd70f0496
Carolynbelly <694284143@qq.com>
2018-05-16 19:33:20 +0800

    提现详情&常见问题&支付成功失败&设置支付验证

---
+++ pages/user/give_details/give_details.js
@@ -0,0 +1,65 @@
+Page({
+
+    data: {
+
+        balanceList: [
+
+            {
+                operation: '提现状态',//
+
+                detail: '银行处理中',
+            },
+
+
+            {
+                operation: '提现金额',//
+
+                detail: '￥500.00',
+
+            },
+
+            {
+                operation: '手续费',//
+
+                detail: '￥0.00',
+            },
+
+            {
+                operation: '支付金额',//
+                detail: '￥500.00',
+            }
+
+        ],//工资明细
+
+
+        balanceList1: [
+
+            {
+                operation: '订单时间',//
+
+                detail: '2018-04-11 16:18:30',
+            },
+
+
+            {
+                operation: '订单号',//
+
+                detail: '123547889990',
+
+            },
+
+            {
+                operation: '银行',//
+
+                detail: '工商银行',
+            },
+
+            {
+                operation: '卡号',//
+                detail: '62719729389060',
+            }
+
+        ]//工资明细
+    }
+
+});
