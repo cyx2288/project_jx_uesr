80f25a018d6be4e57fb74d4060fea2976feaca2b
Carolynbelly <694284143@qq.com>
2018-05-18 18:20:59 +0800

    帮助与客服

--- pages/user/help_service/help_service.js
+++ pages/user/help_service/help_service.js
@@ -1,4 +1,29 @@
-
 Page({
+    openConfirm: function () {
+        wx.showModal({
+            title: '400-888-8888',
 
+            confirmText: "拨打",
+            cancelText: "取消",
+            success: function (res) {
+                console.log(res);
+                if (res.confirm) {
+                    console.log('用户点击主操作')
+                }else{
+                    console.log('用户点击辅助操作')
+                }
+            }
+        });
+    },
+    openAlert: function () {
+        wx.showModal({
+            content: '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内',
+            showCancel: false,
+            success: function (res) {
+                if (res.confirm) {
+                    console.log('用户点击确定')
+                }
+            }
+        });
+    }
 });
