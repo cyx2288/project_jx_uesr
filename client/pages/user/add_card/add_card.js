c78381b209c32e26fcd424bebc0cde6bcaa9130d
zhuangyi28 <zhuangyi28@foxmail.com>
2018-05-21 18:48:08 +0800

    接口调试

---
+++ client/pages/user/add_card/add_card.js
@@ -0,0 +1,629 @@
+/**
+ * Created by ZHUANGYI on 2018/5/14.
+ */
+var addBankList = {
+
+    addBankArray: [
+
+        ['中国银行', '农业银行','建设银行', '交通银行','中国邮政储蓄银行','广发银行','浦发银行','浙江泰隆商业银行'],
+
+        ['储蓄卡', '信用卡'],
+
+    ],
+
+
+
+}
+const app = getApp();
+
+const json2FormFn = require( '../../../static/libs/script/json2Form.js' );//json转换函数
+
+const addBankUrl = '/user/bank/addbankcardinfo';
+
+const getBankName = '/user/bank/getbankname';
+
+const getCity ='/user/bank/citys';
+
+const getProvinces = '/user/bank/provinces';
+
+Page({
+
+    data:{
+
+        bankNo:'',//银行卡号
+
+        openBank:'',//开户行
+
+        bankName:'请选择所属银行',//银行名称&所属银行
+
+        bankBranch:'',//卡户支行
+
+        province:'请选择开户所在地（选填）',//开户省份
+
+        city:'',//开户城市
+
+        userName:'',//用户姓名
+
+        thisBankSort:'',//卡类
+
+        provinceId:'',//省唯一id
+
+        cityArr:[],//城市数组
+
+        provinceArr:[],//区域数组
+
+        countries:[[],[]],//城市数组
+
+        cityData:[],//存储ajax后得到的数组
+
+        provinceData:[],//存储ajax后得到的数组
+
+        multiArray: [
+
+            ['中国银行', '农业银行','建设银行', '交通银行','中国邮政储蓄银行','广发银行','浦发银行','浙江泰隆商业银行'],
+
+            ['储蓄卡', '信用卡']
+
+        ]//银行卡
+
+
+    },
+
+    onLoad:function () {
+
+        var that = this;
+
+        var _userName = wx.getStorageSync('userName');
+
+        var _isVerify = wx.getStorageSync('isVerify');
+
+
+
+        that.setData({
+
+            userName:_userName,
+
+        });
+
+
+        //获取省的方法
+        that.loadCity();
+
+        //获取市的方法
+        that.loadProvince(1);
+
+
+    },
+
+    //获取省
+    loadCity:function () {
+
+        var that = this;
+
+        var thatGetProvince = app.globalData.URL + getProvinces;
+
+        //缓存jx_sid&&Authorization数据
+        var jx_sid = wx.getStorageSync('jxsid');
+
+        var Authorization = wx.getStorageSync('Authorization');
+
+        /**
+         * 接口：获取省地址
+         * 请求方式：GET
+         * 接口：/user/bank/provinces
+         * 入参：null
+         * */
+        wx.request({
+
+            url: thatGetProvince,
+
+            method: 'GET',
+
+            header:{
+
+                'jxsid':jx_sid,
+
+                'Authorization':Authorization
+
+            },
+
+            success: function (res) {
+
+                console.log(res.data);
+
+                var list = res.data.data;
+
+                that.setData({
+
+                    cityData:res.data.data,
+
+                });
+
+                var citylistArr=[];
+
+                //遍历数组 将城市名遍历出来组成新的数组
+
+                for(var i=0;i<list.length;i++){
+
+
+                    var cityList = res.data.data[i].addrName;
+
+                    //组成数组
+                    citylistArr.push(cityList)
+
+                }
+
+                //储存城市
+                that.setData({
+
+                    cityArr:citylistArr
+
+                });
+
+
+                that.setData({
+
+                    countries:[
+
+                        that.data.cityArr,
+
+                        that.data.provinceArr
+
+                    ]
+
+                })
+
+
+
+
+            },
+
+
+            fail: function (res) {
+
+                console.log(res)
+
+            }
+
+        })
+
+
+    },
+
+    //获取市
+    loadProvince:function (thisProvinceId) {
+
+        var that = this;
+
+        var thisGetCity = app.globalData.URL+getCity;
+
+        //缓存jx_sid&&Authorization数据
+        var jx_sid = wx.getStorageSync('jx_sid');
+
+        var Authorization = wx.getStorageSync('Authorization');
+
+        /**
+         * 接口：获取市地址
+         * 请求方式：GET
+         * 接口：/user/bank/citys
+         * 入参：provinceId
+         * */
+        wx.request({
+
+            url: thisGetCity,
+
+            method: 'GET',
+
+            data:{
+
+                provinceId:thisProvinceId//省市唯一地址
+
+            },
+
+            header:{
+
+                'jx_sid':jx_sid,
+
+                'Authorization':Authorization
+
+            },
+
+            success: function (res) {
+
+                console.log(res.data);
+
+                var list = res.data.data;
+
+                var provincelistArr=[];
+
+                for(var i=0;i<list.length;i++){
+
+                    var provinceList = res.data.data[i].addrName;
+
+                    provincelistArr.push(provinceList)
+
+                }
+
+
+                //储存省份
+                that.setData({
+
+                    provinceArr:provincelistArr
+
+                });
+
+                that.setData({
+
+                    countries:[
+
+                        that.data.cityArr,
+
+                        that.data.provinceArr
+
+                    ]
+
+                });
+
+
+
+            },
+
+
+            fail: function (res) {
+
+                console.log(res)
+
+            }
+
+        })
+
+
+
+    },
+
+    //点击添加银行卡号
+    addBankFn:function () {
+
+        var thisAddBankUrl = app.globalData.URL+addBankUrl;
+
+        var that = this;
+
+
+        //缓存jx_sid&&Authorization数据
+        var jx_sid = wx.getStorageSync('jxsid');
+
+        var Authorization = wx.getStorageSync('Authorization');
+
+        var regNeg = /^([1-9]{1})(\d{15}|\d{18})$/;
+
+
+        /**
+         * 接口：添加用户银行卡信息
+         * 请求方式：POST
+         * 接口：/user/bank/addbankcardinfo
+         * 入参：bankNo，openBank，bankName，bankBranch，province，city
+         * */
+
+        wx.request({
+
+            url: thisAddBankUrl,
+
+            method: 'POST',
+
+            data:json2FormFn.json2Form({
+
+                bankNo:that.data.bankNo,//银行卡号
+
+                openBank:that.data.openBank,//开户行
+
+                bankName:that.data.bankName,//银行名称&所属银行
+
+                bankBranch:that.data.bankBranch,//卡户支行
+
+                province:that.data.province,//开户省份
+
+                city:that.data.city//开户城市
+
+            }),
+
+            header:{
+
+                'content-type': 'application/x-www-form-urlencoded', // post请求
+
+                'jxsid':jx_sid,
+
+                'Authorization':Authorization
+
+            },
+
+            success: function (res) {
+
+                console.log(res.data);
+
+                //console.log(that.data.bankNo)
+
+                //银行卡添加成功 toast提示成功
+
+                if(res.data.code=='0000'){
+
+                    wx.showToast({
+
+                        title: res.data.msg,
+                        icon: 'none',
+
+                    })
+
+                    wx.redirectTo({
+
+                        url:'../card/card'
+
+                    })
+
+
+                }
+
+
+                else {
+
+                    //判断银行卡是否为空
+                    if(!that.data.bankNo){
+
+                        wx.showToast({
+
+                            title: '请填写银行卡号',
+                            icon: 'none',
+
+                        })
+
+
+                    }
+
+                    //如果有值的话 判断是否是数字、15位或者18位
+                    else if(that.data.bankNo){
+
+                        //判断卡号是否有误
+                        if(!regNeg.test(that.data.bankNo)){
+
+                            wx.showToast({
+
+                                title: '卡号填写错误',
+
+                                icon: 'none',
+
+                            })
+
+                        }
+
+                        //判断是否写了所属银行
+                        else if(!that.data.bankName){
+
+                            wx.showToast({
+
+                                title: '请选择所属银行',
+                                icon: 'none',
+
+                            })
+                        }
+
+                    }
+
+
+
+                }
+
+
+            },
+
+
+            fail: function (res) {
+                console.log(res)
+            }
+
+        })
+
+
+    },
+
+
+    //监听银行卡号
+    bankNoFn:function (e) {
+
+        var that = this;
+
+        //缓存jx_sid&&Authorization数据
+        var jx_sid = wx.getStorageSync('jxsid');
+
+        var Authorization = wx.getStorageSync('Authorization');
+
+        that.setData({
+
+            bankNo:e.detail.value,
+
+        });
+
+
+    },
+
+    //判断卡号
+    getBankNoFn:function (e) {
+
+        var thisGetBankName = app.globalData.URL+getBankName;
+
+        var that = this;
+
+        //缓存jx_sid&&Authorization数据
+        var jx_sid = wx.getStorageSync('jxsid');
+
+        var Authorization = wx.getStorageSync('Authorization');
+
+        /**
+         * 接口：卡号识别所属银行
+         * 请求方式：POST
+         * 接口：/user/bank/getbankname
+         * 入参：null
+         * */
+
+        wx.request({
+
+         url: thisGetBankName,
+
+         method: 'POST',
+
+         data:{
+
+         bankNo:e.detail.value,
+
+         },
+
+         header:{
+
+         'content-type': 'application/x-www-form-urlencoded', // post请求
+
+         'jxsid':jx_sid,
+
+         'Authorization':Authorization
+
+         },
+
+         success: function (res) {
+
+         console.log(res.data);
+
+         console.log(res.data.data.bankName)
+
+             that.setData({
+
+                 bankName:res.data.data.bankName,
+
+             })
+
+
+         },
+
+
+         fail: function (res) {
+         console.log(res)
+         }
+
+         })
+
+
+
+
+    },
+
+    //通过卡号监听开户行
+    bindMultiPickerChange: function(e) {
+
+        var that = this;
+
+        /*console.log(e.detail.value[0])
+
+        console.log(e.detail.value[1])
+
+        console.log(that.data.multiArray[0])
+
+        console.log(that.data.multiArray[1])
+
+        console.log(that.data.multiArray[0][e.detail.value[0]])
+
+        console.log(that.data.multiArray[1][e.detail.value[1]])*/
+
+        that.setData({
+
+            bankName:that.data.multiArray[0][e.detail.value[0]],
+
+            thisBankSort:that.data.multiArray[1][e.detail.value[1]]
+
+
+        })
+
+
+
+
+
+
+    },
+
+    bindMultiPickerColumnChange: function (e) {
+
+        //console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+
+    },
+
+    bindRegionColumnChange:function (e) {
+
+        var that = this;
+
+        console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+
+        //判断是否选择的是第一列
+
+        if(e.detail.column==0){
+
+            //获取选中的城市的uniqueId
+
+            var whichCityId = that.data.cityData[e.detail.value].uniqueId;
+
+            that.loadProvince(whichCityId)
+
+        }
+
+
+
+
+    },
+
+    //确定后值的回显
+    bindRegionChange: function (e) {
+
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+
+        var that = this;
+
+   /*   console.log(e.detail.value[0]);
+
+        console.log(e.detail.value[1])
+
+        console.log(that.data.cityArr);
+
+        console.log(that.data.provinceArr)
+
+        console.log(that.data.cityArr[e.detail.value[0]]);
+
+        console.log(that.data.provinceArr[e.detail.value[1]])
+        */
+
+        that.setData({
+
+            city:that.data.cityArr[e.detail.value[0]]+',',//开户城市
+
+            province:that.data.provinceArr[e.detail.value[1]],//开户省份
+
+
+        })
+
+
+
+    },
+
+    //获取开户支行
+    getBankBranchFn:function (e) {
+
+        var that = this;
+
+        that.setData({
+
+            openBank: e.detail.value,
+
+            bankBranch:e.detail.value,
+
+
+        })
+
+    }
+
+
+
+
+})
---
+++ client/pages/user/cash/cash.js
@@ -0,0 +1,475 @@
+const app = getApp();
+
+const json2FormFn = require('../../../static/libs/script/json2Form.js');//json转换函数
+
+const cashUrl = '/user/withdraw/dowithdraw';
+
+const checkcashUrl = '/user/work/checkwithdraw';
+
+
+Page({
+
+    data: {
+
+        bizId: '',//订单id
+
+        bankCardId: '',//银行卡id
+
+        balance: '',//提取现金
+
+        inputBalance:'',//输入框里value的值
+
+        canCashBalance:'',//可以提现的金额
+
+        payPassword: '',//支付密码
+
+        code: '',//短信验证
+
+        bankList: [],//银行卡列表数组
+
+        bankName: '',//银行名称
+
+        bankNo: '',//银行卡号
+
+        chooseBank: [],//picker中银行卡的数组
+
+        amountMax: '',//单笔最大限额
+
+        amountMin: '',//单笔最小限额
+
+        dayMaxCount: '',//日最大额度
+
+        monthMaxAmount: '',//月最大额度
+
+        rate: '',//费率
+
+    },
+
+    onLoad: function () {
+
+        var that = this;
+
+
+        var thisCheckcashUrl = app.globalData.URL + checkcashUrl;
+
+        //获取银行卡页面的银行卡
+        var jx_sid = wx.getStorageSync('jxsid');
+
+        var Authorization = wx.getStorageSync('Authorization');
+
+        var thisBankList = wx.getStorageSync('bankList');
+
+        var _isVerify = wx.getStorageSync('isVerify');
+
+        //没认证的去认证
+        if (_isVerify == '0') {
+
+            wx.showModal({
+                title: '提示',
+                content: ' 未完成实名认证的用户，需先完成实名认证才可添加银行卡',
+                cancelText: '取消',
+                confirmText: '去认证',
+                success: function (res) {
+
+                    if (res.confirm) {
+
+                        wx.navigateTo({
+
+                            url: '../certification/certification'
+
+                        })
+
+                    }
+
+                    else if (res.cancel) {
+
+
+                    }
+                }
+            });
+
+
+        }
+
+        //存储银行卡页面的数据
+        that.setData({
+
+            bankList: thisBankList,
+
+        });
+
+        //没银行卡的去添加银行卡
+        if (that.data.bankList.length == 0) {
+
+
+            wx.showModal({
+                title: '提示',
+                content: ' 您还没有可用于提现的银行卡，请先添加一张储蓄卡',
+                cancelText: '取消',
+                confirmText: '去添加',
+                success: function (res) {
+
+                    if (res.confirm) {
+
+                        wx.navigateTo({
+
+                            url: '../add_card/add_card'
+
+                        })
+
+                    }
+
+                    else if (res.cancel) {
+
+
+                    }
+                }
+            });
+
+
+        }
+
+
+        //默认显示第一个银行卡
+        that.setData({
+
+            bankName: that.data.bankList[0].bankName,//银行名称
+
+            bankNo: that.data.bankList[0].bankNo,//银行卡号
+
+            bankCardId: that.data.bankList[0].bankCardId//银行卡id
+
+        });
+
+
+        //获取银行卡的
+        var pickChooseBank = [];
+
+        //循环银行卡、银行名称及银行id
+        for (var i = 0; i < thisBankList.length; i++) {
+
+            var pickBankName = thisBankList[i].bankName;
+
+            var pickBankNo = thisBankList[i].bankNo;
+
+            var pickBankId = thisBankList[i].bankCardId;
+
+            var _pickChooseBank = pickBankName + '（储蓄卡） ' + pickBankNo;
+
+            //组成数组
+            pickChooseBank.push(_pickChooseBank);
+
+        }
+
+        console.log(pickChooseBank);
+
+        that.setData({
+
+            chooseBank: pickChooseBank
+
+        })
+
+        /**
+         * 接口：检测用户发起提现操作
+         * 请求方式：GET
+         * 接口：/user/work/checkwithdraw
+         * 入参：null
+         * */
+
+        wx.request({
+
+            url: thisCheckcashUrl,
+
+            method: 'GET',
+
+            data: {
+
+                code: that.data.code,
+
+
+            },
+            header: {
+
+                'jx_sid': jx_sid,
+
+                'Authorization': Authorization
+
+            },
+
+            success: function (res) {
+
+                console.log(res.data);
+
+                that.setData({
+
+                    balance: res.data.data.balance,//提取现金
+
+                    amountMax: res.data.data.amountMax,//单笔最大限额
+
+                    amountMin: res.data.data.amountMin,//单笔最小限额
+
+                    dayMaxCount: res.data.data.dayMaxCount,//日最大额度
+
+                    monthMaxAmount: res.data.data.monthMaxAmount,//月最大额度
+
+                    rate: res.data.data.rate,//费率
+
+                    canCashBalance:res.data.data.balance,
+
+                })
+
+
+            },
+
+
+            fail: function (res) {
+
+                console.log(res)
+
+            }
+
+        })
+
+    },
+
+    //赋值
+    bindPickerChange: function (e) {
+
+        var that = this;
+
+        //选择银行卡的
+        //console.log(that.data.chooseBank[e.detail.value]);
+
+        that.setData({
+
+            bankName: that.data.bankList[e.detail.value].bankName,//银行名称
+
+            bankNo: that.data.bankList[e.detail.value].bankNo,//银行卡号
+
+            bankCardId: that.data.bankList[e.detail.value].bankCardId//银行卡id
+
+        });
+
+
+    },
+
+    //全部提现
+    cashAllFn: function () {
+
+        var that = this;
+
+        that.setData({
+
+            inputBalance: that.data.balance,
+
+        })
+
+
+    },
+
+    //提现
+    cashFn: function () {
+
+        var thisCashUrl = app.globalData.URL + cashUrl;
+
+        var that = this;
+
+        //缓存jx_sid&&Authorization数据
+        var jx_sid = wx.getStorageSync('jx_sid');
+
+        var Authorization = wx.getStorageSync('Authorization');
+
+        console.log('金额：'+that.data.inputBalance)
+
+        if(!that.data.inputBalance){
+
+            wx.showToast({
+                title: '请输入金额',
+                icon: 'none',
+                duration: 1000
+            })
+
+        }
+
+        else {
+
+            wx.showModal({
+
+                title: '确认付款',
+                content: '支付金额' + that.data.balance + ',提现金额￥100.00,手续费￥0.00',
+                confirmText: '确认付款',
+
+                success: function (res) {
+
+                    if (res.confirm) {
+
+                        confirmation()
+
+                    }
+
+                    else if (res.cancel) {
+
+
+                    }
+                }
+            });
+
+
+        }
+
+
+
+
+        function confirmation() {
+            /**
+             * 接口：获取用户银行卡信息
+             * 请求方式：GET
+             * 接口：/user/withdraw/dowithdraw
+             * 入参：bizId,bankCardId,balance,payPassword,code
+             * */
+
+            wx.request({
+
+                url: thisCashUrl,
+
+                method: 'GET',
+
+                data: {
+
+
+                    bizId: that.data.bizId,//订单id
+
+                    bankCardId: that.data.bankCardId,//银行卡id
+
+                    balance: that.data.balance,//提取现金
+
+                    payPassword: that.data.payPassword,//支付密码
+
+                    code: that.data.code,//短信验证
+
+
+                },
+                header: {
+
+                    'jx_sid': jx_sid,
+
+                    'Authorization': Authorization
+
+                },
+
+                success: function (res) {
+
+                    console.log(res.data);
+
+                    if (res.data.code == '0000') {
+
+
+                    }
+
+                    //如果
+
+                    else {
+
+                        if (!that.data.balance) {
+
+                            wx.showToast({
+
+                                title: '请输入金额',
+                                icon: 'none',
+
+                            })
+
+                        }
+
+                    }
+
+
+                },
+
+
+                fail: function (res) {
+
+                    console.log(res)
+
+                }
+
+            })
+        }
+
+
+    },
+
+    hasTipsFn: function () {
+
+        wx.showModal({
+            title: '提示限额说明',
+            content: '单卡单笔50000.00,当日10000000.00,当月3000000.00元',
+            confirmText: '确认',
+            showCancel: false,
+            success: function (res) {
+
+                if (res.confirm) {
+
+
+                }
+
+                else if (res.cancel) {
+
+                }
+            }
+        });
+
+
+    },
+
+    moreFn: function () {
+
+        wx.showActionSheet({
+            itemList: ['提现记录', '常见问题'],
+            success: function (res) {
+
+                console.log(res.tapIndex)
+
+                if (res.tapIndex == '0') {
+
+                    wx.navigateTo({
+
+                        url: '../bill/bill'
+                    })
+
+                }
+
+                else if (res.tapIndex == '1') {
+
+                    wx.navigateTo({
+
+                        url: '../help_service/help_service'
+                    })
+
+                }
+
+            },
+            fail: function (res) {
+
+            }
+        })
+
+    },
+
+    bindKeyInput: function (e) {
+
+        var that = this;
+
+        that.setData({
+
+            balance: e.detail.value,
+
+            inputBalance:e.detail.value,
+
+
+        });
+
+
+    }
+
+});
