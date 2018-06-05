c596979887c47554f44d1c336cef393bebf08436
zhuangyi28 <zhuangyi28@foxmail.com>
2018-06-06 00:11:13 +0800

    更新

--- client/app.js
+++ client/app.js
@@ -4,11 +4,13 @@ App({
 
     userInfo: null,
 
-    /*URL:'http://192.168.66.177:8091/jx-user'//环境*/
+      /*URL:'http://192.168.66.177:8091/jx-user'//环境*/
 
+      /*URL:'https://user.99payroll.cn/jx-user',*///生产环境
 
+      URL:'http://jxtest.99payroll.cn/jx-user',
 
-    /*URL:'http://172.18.1.62:8091/jx-user'*/
+      /*URL:'http://172.18.1.62:8091/jx-user'*/
 
     ajaxFinish:function (ajaxCount) {
 
