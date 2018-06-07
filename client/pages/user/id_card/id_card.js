b23a578619d30f97aa53e90e0f6286cad9323dbc
zhuangyi28 <zhuangyi28@foxmail.com>
2018-06-07 22:02:21 +0800

    接口

--- client/pages/user/id_card/id_card.js
+++ client/pages/user/id_card/id_card.js
@@ -67,7 +67,7 @@ Page({
 
         var check = /^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/;
 
-        console.log(_tokenMsg);
+        //console.log(_tokenMsg);
 
 
 
